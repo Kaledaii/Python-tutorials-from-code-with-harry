1bff4ad752fab0e3c29ffd8d4550da566e13915c
Kaledaii <mishant480@gmail.com>
2025-09-14 19:07:14 +0545

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,7 @@
+list=["ram","shyam","hari"]
+
+name=input("Enter your name: ").lower()
+if (name in list):
+    print("Your name is present in the list")   
+else:
+    print("Your name is not present in the list")
